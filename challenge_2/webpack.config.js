8bae07242097492f067e4405e2d193d4f0f3a07a
akwong372 <akwong372@gmail.com>
2019-05-21 12:10:46 -0700

    [C2] Sets up webpack and express

---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    ]
+  }
+}
