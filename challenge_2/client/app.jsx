803f6ef25348817723846bc41e9cd3596e47fe6d
akwong372 <akwong372@gmail.com>
2019-05-21 18:52:37 -0700

    [C2] Creates canvas with 2d context

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -4,11 +4,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {}
+    this.chartRef = React.createRef();
   }
 
   render() {
+
     return (
       <div>
+        <canvas ref={(c) => {const thing = this.chartRef = c.getContext('2d'); console.log(thing)}} id='chartCanvas' width='400' height='400'></canvas>
         test app
       </div>
     )
