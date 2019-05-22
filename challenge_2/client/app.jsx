ed2569aacae17b36aeb6c68ff3ae2f76b7ba0278
akwong372 <akwong372@gmail.com>
2019-05-21 19:44:05 -0700

    [C2] Gets and displays data from coindesk api

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Chart from 'chart.js';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      cryptocurrency: 'BTC',
+      dateStart: '2019-01-01',
+      dateEnd: '2019-05-01',
+      chartType: 'line',
+      dataLabels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      dataPts: [12, 19, 3, 5, 2, 3],
+      disclaimer: ''
+    }
 
     this.chartRef = React.createRef();
     this.createChart = this.createChart.bind(this);
@@ -12,29 +21,17 @@ class App extends React.Component {
 
   createChart() {
     return new Chart(this.ctx, {
-      type: 'bar',
+      type: this.state.chartType,
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: this.state.dataLabels,
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
+          label: this.state.cryptocurrency,
+          data: this.state.dataPts,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 0.7)',
+          borderWidth: 1,
+          fill: false,
+          steppedLine: true
         }]
       },
       options: {
@@ -51,14 +48,25 @@ class App extends React.Component {
 
   componentDidMount() {
     this.ctx = this.chartRef.current.getContext('2d');
-    this.createChart();
+    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${this.state.dateStart}&end=${this.state.dateEnd}`)
+      .then((res) => {
+        this.setState({
+          dataLabels: Object.keys(res.data.bpi),
+          dataPts: Object.values(res.data.bpi),
+          disclaimer: res.data.disclaimer
+        });
+      })
+      .then(() => this.createChart())
+      .catch((err) => console.log('Error in axios get ', err));
   }
 
   render() {
     return (
       <div>
         <canvas ref={this.chartRef} id='chartCanvas' width='400' height='400'></canvas>
-        test app
+        <div>
+          {this.state.disclaimer}
+        </div>
       </div>
     )
   }
