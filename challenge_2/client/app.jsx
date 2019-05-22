c198afdd8e2bc47997f6fc5a311c57380a826366
akwong372 <akwong372@gmail.com>
2019-05-21 19:09:34 -0700

    [C2] Creates working chart with test data

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,17 +1,63 @@
 import React from 'react';
+import Chart from 'chart.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {}
+
     this.chartRef = React.createRef();
+    this.createChart = this.createChart.bind(this);
+  }
+
+  createChart() {
+    return new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.ctx = this.chartRef.current.getContext('2d');
+    this.createChart();
   }
 
   render() {
-
     return (
       <div>
-        <canvas ref={(c) => {const thing = this.chartRef = c.getContext('2d'); console.log(thing)}} id='chartCanvas' width='400' height='400'></canvas>
+        <canvas ref={this.chartRef} id='chartCanvas' width='400' height='400'></canvas>
         test app
       </div>
     )
