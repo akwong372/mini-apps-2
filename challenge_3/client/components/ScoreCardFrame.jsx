5e5625a46003f54a10d15cc029698c996adb110c
akwong372 <akwong372@gmail.com>
2019-05-28 15:15:17 -0700

    [C3] Displays proper symbol for spares

--- challenge_3/client/components/ScoreCardFrame.jsx
+++ challenge_3/client/components/ScoreCardFrame.jsx
@@ -4,7 +4,7 @@ const scoreCardFrame = (props) => (
   <tr>
     <td>{props.score}</td>
     <td>{props.try1}</td>
-    <td>{props.try2}</td>
+    <td>{props.try2 + props.try1 === 10 ? '/' : props.try2}</td>
   </tr>
 );
 
